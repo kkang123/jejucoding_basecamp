da77019277e1fc6bda0573b7093b27a205cf4f0b
Ubuntu <ubuntu@ip-172-31-42-133.ap-northeast-2.compute.internal>
2022-09-03 08:06:42 +0000

    reslove_all_FE,BE,AWS_errors

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''.join([random.SystemRandom().choice(chars) for i in range(50)])
+SECRET_KEY = '12345678901234567890123456789012345678901234567890'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
